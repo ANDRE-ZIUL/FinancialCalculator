1e82c255d33465c552fa20da78ab03868c860539
ANDRE-ZIUL <dedelcdias@gmail.com>
2024-05-13 19:52:27 -0300

    feat: adjust for initial project

---
+++ src/Interface/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const FinancialCalculator: React.FC = () => {
+  return (
+    <div className="financial-calculator">
+      <h1>Aqui vai existir uma calculadora financeira</h1>
+    </div>
+  );
+};
